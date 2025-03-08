7f3646e3956fe1b0485be2cab6679c5d3fa939d3
ysk0413 <watanabeyusuke93@gmail.com>
2025-03-08 15:18:49 +0900

    Create Unregistered.vue

---
+++ Unregistered.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-text-center" style="margin-top: 2rem;">
+    <h2>未登録</h2>
+    <p>まだ会員登録がお済みではありません。</p>
+    <Button label="会員登録する" icon="pi pi-user-plus" @click="goToRegister" class="p-mt-2" />
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+import Button from 'primevue/button'
+
+const router = useRouter()
+
+function goToRegister() {
+  router.push({ name: 'Register' })
+}
+</script>
+
+<style scoped>
+</style>
