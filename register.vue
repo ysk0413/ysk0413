d96540d1a51dd3d3dae4a0d5508a68e7a6f7a613
ysk0413 <watanabeyusuke93@gmail.com>
2025-03-08 15:16:36 +0900

    Create register.vue

---
+++ register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="register-page p-mt-4 p-px-3">
+    <h2 class="p-text-center">会員登録</h2>
+    <RegisterForm />
+  </div>
+</template>
+
+<script setup>
+import RegisterForm from '../components/RegisterForm.vue'
+</script>
+
+<style scoped>
+.register-page {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
